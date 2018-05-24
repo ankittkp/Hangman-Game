e2ee2cc74ed281e2cf4d0526bb6a75a222113f0e
Ankit Kumar <ankit.2official@gmail.com>
2018-05-24 20:27:43 +0530

    updated code

--- hangman.c
+++ hangman.c
@@ -1,94 +1,222 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-char string[100],hangman_string[100],result[100],input_char;
-int wrong_chance = 6,match_found=0;
-int count=0,pos=0,winner,len,i,j;
+#include<string.h>   
+    char char_input;
+    char original_input[100];
+    char compare_string[100];
+    char answer[100];
+    int mistakes = 6;
+    int count_match= 0;
+    int count=0;
+    int pos= 0;
+    int winner;
+    int length;
+    int i;
+void model_hangman(int);
 int main()
 {
+    system("clear");
+    printf("\n\n\t\tEnter a word that you want to guess by your friend and hit Enter <<  .......");
+    printf("\n\n\n      Input HERE        :-         ");
+    scanf("%s",original_input);
+    printf("\n");
+    printf("\n\n Now pass your laptop to your friend and see if he/she'll going to live or die xD .....!!!!!");
+    system("clear");
+    getchar();                      
+    length = strlen(original_input);
+    system("clear");
+    printf("\n\n\n\n\n\n ##############################   WELCOME TO THE HANGMAN GAME    ############################\n\n\n");
+    printf("\n\n\tYou will get 5 chances to guess the right word, unless you'll going to die :(  ");
 
+    getchar();
 
-	printf("\n\nEnter any word ('small cases only') to guess by other player : ");
-	scanf("%s",string);
-	printf("Now, Game time \n");
-	printf("Please Pass the screen to the player : \n");
-	getchar();
+    printf("\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t   Press Enter for Continue........ ");
 
-	len=strlen(string);
+    getchar();
 
-	//system("cls");
+    system("clear");
+        printf("\n\t\t\t\t||==========   ");
+	       printf("\n\t\t\t\t||     | ");
+        printf("\n\t\t\t\t||      ");
+        printf("\n\t\t\t\t||      ");
+        printf("\n\t\t\t\t||      ");
+        printf("\n\t\t\t\t||      ");
+        printf("\n\t\t\t\t||      ");
+        printf("\n\t\t\t\t||      ");
 
-	printf("\n\n\n-----------------------Welcome to Hangman Game---------------------\n\n\n");
-	printf("\n\n\nyou will get 5 chance to guess the word \n\n\n");
-	getchar();
-	printf("\n\n\tHIT >>ENTER<< ");
-	getchar();
-
-	//system(cls);
-
-	printf("\n\t||===== ");               
-	printf("\n\t||    | ");
-    printf("\n\t||      ");
-    printf("\n\t||      ");
-    printf("\n\t||      ");
-    printf("\n\t||      ");
-
-    printf("\n\n     The word has %d alphabets \n\n",len);
-
-    for( i = 0; i < len ; i++)
+    printf("\n\n\n\n    The word that your friend entered has %d alphabets \n\n",length);
+    for( i = 0; i < length ; i++)
     {
-        result[i] = '_';
-        result[len] = '\0';
-	}
-	for(i = 0 ; i < len ; i++)
+        answer[i] = '_';
+        answer[length] = '\0';
+    }
+    printf("\n\t");
+    for(i = 0 ; i < length ; i++)
     {
         printf(" ");
-        printf("%c",result[i]);
-	}
-	while(wrong_chance!=0)
-	{
-		match_found=0;
-		printf("\n\nEnter any alphabet to play which is in small case : ");
-		scanf("%c",&input_char);
+        printf("%c",answer[i]);
 
-		if(input_char < 'a' || input_char > 'z')
-		{
-			printf("\n\n\t Wrong input TRY AGAIN ");
-			match_found = 2;
-		}
+    }
+    printf("\n\n\n");
+    while(mistakes != 0)
+    {
+        count_match = 0;
+        printf("\n\n   enter any alphabet that you want to guess !!");
+        printf("\n\n\n      Input HERE        :-         ");
+	    fflush(stdin);
+	    scanf("%c",&char_input);
+    if(char_input < 'a' || char_input > 'z')
+    {
+        system("clear");
+        printf("\n\n\t Wrong guess TRY AGAIN ");
+        count_match = 2;
+    }
+    fflush(stdin);
+    if (count_match != 2)
+    {
+        for(count=0;count<length;count++)
+	    {
+		    if(char_input==original_input[count])
+		     {
+		       count_match = 1;
+                     }
+            }
 
-		if(match_found!=2)
-		{
-			for(count=0;count<len;count++)
-			{
-				if(input_char==string[count])
-				{
-					match_found=1;
+	   if(count_match == 0)
+	    {
+     	      printf("\n\t :( You have only %d tries left, try something else \n\n",--mistakes);
+	          getchar();
+              model_hangman(mistakes);
+              getchar();
+	    }
 
-				}
-			}
+	   else
+	   {
+	     for(count = 0; count < length; count++)
+             {
+     	         count_match = 0;
+                 if(char_input == original_input[count])
+	          {
+     		     pos = count ;
+     		     count_match = 1;
+	          }
+    	      if(count_match == 1)
+	          {
+                 for(i = 0 ; i < length ; i++)
+                 {
+                      if( i == pos)
+                  	  {
+                          answer[i] = char_input;
+                      }
+                      else if( answer[i] >= 'a' && answer[i] <= 'z' ) 
+                      {
+                          continue;
+                  	  }
 
+                      else
+                      {
+                          answer[i] = '_';            
+                      }
+                }
+                compare_string[pos] = char_input;      
+                compare_string[length] = '\0';                    
+                winner = strcmp(compare_string,original_input);     
 
-			if(match_found==0)
-			{
+                if(winner == 0)                             
+                {
+                    printf("\n\n\t \t Hurray !!!!! You are the WINNER :) :) :) ");
+                    printf("\n\n\t The Word was %s ",original_input);
+                    getchar();
+                    return 0;
+                }
+	       }
+	    }
+      }
+     }
 
-				printf("\n\t :( You have %d tries left ",--wrong_chance);
-				getchar();
-				//Hangman(wrong_chance);
-				getchar();
+    printf("\n\n\t");
+    for(i = 0 ; i < length ; i++)
+      {
+          printf(" ");
+          printf("%c",answer[i]);       
+      }
 
-			}
+    getchar();
+    }
 
-			else
-			{
-				for(count=0;count<len;count++)
-				{
-					match_found=0;
-									}
-			}
-		}
-	}
+      if(mistakes <= 0)      
+      {
+          printf("\n\n\t The Word was %s \n",original_input);
+          printf("\n\n\t The man is dead you moron :( :( :( ");
+	      printf("\n\n\t Better luck next time :) :) :) \n ");
 
-
-	return 0;
+      }
+getchar();
+return 0;
 }
+
+
+
+void model_hangman(int choice)
+ {
+
+     switch(choice)
+     {
+
+     case 0:
+         system("clear");
+	printf("\n\t\t\t\t||===== ");
+	printf("\n\t\t\t\t||    | ");
+	printf("\n\t\t\t\t||   %cO/",'\\');
+	printf("\n\t\t\t\t||    | ");
+	printf("\n\t\t\t\t||   / %c",'\\');
+	printf("\n\t\t\t\t||      ");
+	break;
+     case 1:
+         system("clear");
+	printf("\n\t\t\t\t||===== ");
+	printf("\n\t\t\t\t||    | ");
+	printf("\n\t\t\t\t||   %cO/",'\\');
+	printf("\n\t\t\t\t||    | ");
+	printf("\n\t\t\t\t||     %c",'\\');
+	printf("\n\t\t\t\t||      ");
+	break;
+     case 2:
+         system("clear");
+	printf("\n\t\t\t\t||===== ");
+	printf("\n\t\t\t\t||    | ");
+	printf("\n\t\t\t\t||   %cO/",'\\');
+	printf("\n\t\t\t\t||    | ");
+	printf("\n\t\t\t\t||      ");
+	printf("\n\t\t\t\t||      ");
+	break;
+     case 3:
+         system("clear");
+	printf("\n\t\t\t\t||===== ");
+	printf("\n\t\t\t\t||    | ");
+	printf("\n\t\t\t\t||   %cO/",'\\');
+	printf("\n\t\t\t\t||      ");
+	printf("\n\t\t\t\t||      ");
+	printf("\n\t\t\t\t||      ");
+	break;
+     case 4:
+         system("clear");
+	printf("\n\t\t\t\t||===== ");
+	printf("\n\t\t\t\t||    | ");
+	printf("\n\t\t\t\t||   %cO ",'\\');
+	printf("\n\t\t\t\t||      ");
+	printf("\n\t\t\t\t||      ");
+	printf("\n\t\t\t\t||      ");
+	break;
+     case 5:
+         system("clear");
+	printf("\n\t\t\t\t||===== ");
+	printf("\n\t\t\t\t||    | ");
+	printf("\n\t\t\t\t||    O ");
+	printf("\n\t\t\t\t||      ");
+	printf("\n\t\t\t\t||      ");
+	printf("\n\t\t\t\t||      ");
+	break;
+      }
+return;
+ }
